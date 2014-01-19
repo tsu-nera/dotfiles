6c063bca0b1198ca2f71a761436c7769a1d9cc58
tsu-nera <fox10225fox@gmail.com>
2014-01-19 20:38:00 +0900

    urxvt設定

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,10 +1,49 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+import XMonad.Actions.WindowGo
+
+import XMonad.Layout.MultiToggle
+import XMonad.Layout.MultiToggle.Instances
+import XMonad.Config.Desktop (desktopLayoutModifiers)
+import XMonad.Layout.Named
+
+-- for keyboard shortcut
+modm = mod4Mask
+
+tall = Tall 1 (3/100) (1/2)
 
 main = do
-     xmonad $ defaultConfig {
-    modMask  = mod4Mask,
-    terminal = "urxvt"
-  } 
+        xmproc <- spawnPipe "/usr/bin/xmobar /home/tsu-nera/.xmobarrc"
+        xmonad $ defaultConfig
+          { manageHook = manageDocks <+> manageHook defaultConfig
+          --, layoutHook = avoidStruts  $  layoutHook defaultConfig
+	  , layoutHook = mkToggle1 FULL $ desktopLayoutModifiers (named "V" tall ||| (named "H" $ Mirror tall))
 
+          , logHook = dynamicLogWithPP $ xmobarPP
+          { ppOutput = hPutStrLn xmproc
+             , ppTitle = xmobarColor "green" "" . shorten 50
+          }
 
+          -- Border settings
+ 	  , borderWidth = 2
+          , normalBorderColor  = "#99ccff"
+          , focusedBorderColor = "#0033dd" -- blue
 
+          -- Rebind Mod to the Hiragana_Katakana key
+          , modMask = mod3Mask
+
+     	  -- use rxvt-unicode 
+	  , terminal = "urxvt" 
+          } 
+          `additionalKeys`
+          [
+          ((modm, xK_e), runOrRaise "emacs" (className =? "Emacs"))
+          , ((modm, xK_t), runOrRaise "urxvt" (className =? "URxvt"))
+          , ((modm, xK_g), runOrRaise "chrome" (className =? "Google-chrome"))
+	  , ((mod3Mask, xK_f), sendMessage (Toggle FULL))
+          ]
