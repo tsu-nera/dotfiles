79621408da4767c8fd030ad2fc64a606a6478098
tsu-nera <fox10225fox@gmail.com>
2014-02-10 14:25:24 +0900

    bindkey変更

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -48,6 +48,8 @@
 (global-linum-mode t)   ;; 行番号の表示
 ;; (global-hl-line-mode 1) ;; 現在行に色をつける
 
+(setq x-select-enable-clipboard t) ;; OSとのクリップボード共有
+
 ;; general key bind
 (global-set-key (kbd "C-c a")   'align)
 (global-set-key (kbd "C-c M-a") 'align-regexp)
@@ -527,6 +529,57 @@
 (add-hook 'c-mode-hook
 	  '(lambda ()
 	     (flymake-mode t)))(require 'flymake)
+
+
+;; -----------------------------------------------------------------------
+;; Name     : org-capture
+;; Function : アイデアをキャプチャーする
+;; History  : 2014/02/25
+;; Install  : build-in
+;; ------------------------------------------------------------------------
+(require 'org-capture)
+(setq org-capture-templates
+      '(
+;;	("t" "Task" entry (file+headline nil "Inbox")
+;;	 "** TODO %?\n %T\n %a\n %i\n")
+;;	("b" "Bug" entry (file+headline nil "Inbox")
+;;	 "** TODO %?   :bug:\n  %T\n %a\n %i\n")
+;;	("m" "Meeting" entry (file+headline nil "Meeting")
+;;	 "** %?\n %U\n %a\n %i\n")
+	("i" "Idea" entry (file+headline nil "~/diary/org/idea.org")
+	 "** %?\n %U\n %i\n %a\n %i\n")
+	("w" "Twitter" entry (file+datetree "~/diary/org/twitter.org")
+	 "** %U %?\n")
+	)
+      )
+(global-set-key (kbd "C-c c") 'org-capture)
+;; -----------------------------------------------------------------------
+;; Name     : yasnippet
+;; Function : スニペット管理
+;; History  : 2014/02/11
+;; Install  : elpa
+;; ------------------------------------------------------------------------
+(require 'cl)
+;; 問い合わせを簡略化 yes/no を y/n
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(require 'yasnippet)
+;;(setq yas-snippet-dirs
+;;      '("~/.emacs.d/snippets"
+;;        ))
+(yas-global-mode 1)
+
+;; 単語展開キーバインド (ver8.0から明記しないと機能しない)
+;; (setqだとtermなどで干渉問題ありでした)
+;; もちろんTAB以外でもOK 例えば "C-;"とか
+(custom-set-variables '(yas-trigger-key "TAB"))
+
+;; 既存スニペットを挿入する
+(define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
+;; 新規スニペットを作成するバッファを用意する
+(define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
+;; 既存スニペットを閲覧・編集する
+(define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
 
 ;; -----------------------------------------------------------------------
 ;; Name     :
