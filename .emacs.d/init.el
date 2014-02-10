c8e287c86024fb63eabf75c42d54fdfb32ce5efc
tsu-nera <fox10225fox@gmail.com>
2014-02-11 07:17:28 +0900

    el-get.elをいれた

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -121,6 +121,21 @@
 (package-initialize)
 
 ;; ------------------------------------------------------------------------
+;; Name     : el-get.el
+;; Function : eLisp管理
+;; Install  : cd public_repos 
+;;            git clone git@github.com:dimitri/el-get.git
+;; ------------------------------------------------------------------------
+(setq el-get-dir "~/.emacs.d/elisp/el-get/")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+(el-get 'sync)
+;; ------------------------------------------------------------------------
 ;; face-display Setting
 ;; ------------------------------------------------------------------------
 ;;; 色を設定する
