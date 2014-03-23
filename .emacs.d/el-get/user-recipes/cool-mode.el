2982b587ff1461868cd80d1b2114f901d839f09d
tsu-nera <fox10225fox@gmail.com>
2014-03-23 19:51:54 +0900

    cool-mode追加

---
+++ .emacs.d/el-get/user-recipes/cool-mode.el
@@ -0,0 +1,5 @@
+(:name cool-mode
+       :type git
+       :website "https://github.com/markxnelson/cool-mode"
+       :description "Major Mode for Cool"
+       :url "https://github.com/markxnelson/cool-mode.git")
---
+++ .emacs.d/inits/33_minorlang.el
@@ -0,0 +1,15 @@
+;; -----------------------------------------------------------------------
+;; Name     :  Oz
+;; Function :  マルチパラダイム言語
+;; ------------------------------------------------------------------------
+(add-hook 'oz-mode-hook
+	  (lambda ()
+	    (define-key oz-mode-map "\C-c\C-b" 'oz-feed-buffer)
+	    (define-key oz-mode-map "\C-c\C-l" 'oz-feed-line)
+	    (define-key oz-mode-map "\C-c\C-r" 'oz-feed-region)))
+;; -----------------------------------------------------------------------
+;; Name     :  Cool
+;; ------------------------------------------------------------------------
+(autoload 'cool-mode "cool-mode" "Major mode for editing COOL programs" t)
+(setq auto-mode-alist
+            (append '(("\\.cl\\'" . cool-mode)) auto-mode-alist))
