6b2ea0506b037666d7ba005380cd7bcf64065915
tsu-nera <fox10225fox@gmail.com>
2014-03-30 10:05:50 +0900

    emacs color修正

--- .emacs.d/themes/molokai-theme-kit.el
+++ .emacs.d/themes/molokai-theme-kit.el
@@ -44,58 +44,59 @@
 ;;
 ;; http://emacs-fu.blogspot.com/2011/08/customizing-mode-line.html
 (setq-default mode-line-format
-              (list
+  (list
 	       
-               ;; the buffer name; the file name as a tool tip
-               " " '(:eval (propertize "%b " 'face 'font-lock-keyword-face 'help-echo (buffer-file-name)))
+   ;; the buffer name; the file name as a tool tip
+   " " '(:eval (propertize "%b " 'face 'font-lock-keyword-face 'help-echo (buffer-file-name)))
 
-	       "("
-	       ;; 文字コード
-		mode-line-mule-info
-	       ")"
+   "("
+   ;; 文字コード
+   mode-line-mule-info
+   ")"
 	       
-               ;; line and column
-               "(" (propertize "%l" 'face 'font-lock-type-face) "," (propertize "%c" 'face 'font-lock-type-face) ") "
+   ;; line and column
+   "(" (propertize "%l" 'face 'font-lock-type-face) "," (propertize "%c" 'face 'font-lock-type-face) ") "
 
-               ;; relative position, size of file
-               "[" (propertize "%p" 'face 'font-lock-constant-face) "/" (propertize "%I" 'face 'font-lock-constant-face) "] "
+   ;; relative position, size of file
+   "[" (propertize "%p" 'face 'font-lock-constant-face) "/" (propertize "%I" 'face 'font-lock-constant-face) "] "
 
-               ;; the current major mode for the buffer.
-               "[" '(:eval (propertize "%m" 'face 'font-lock-string-face 'help-echo buffer-file-coding-system)) "] "
+   ;; the current major mode for the buffer.
+   "[" '(:eval (propertize "%m" 'face 'font-lock-string-face 'help-echo buffer-file-coding-system)) "] "
 
 
-               "[" ;; insert vs overwrite mode, input-method in a tooltip
-               '(:eval (propertize (if overwrite-mode "Ovr" "Ins")
-                                   'face 'font-lock-preprocessor-face
-                                   'help-echo (concat "Buffer is in "
-                                                      (if overwrite-mode "overwrite" "insert") " mode")))
+   "[" ;; insert vs overwrite mode, input-method in a tooltip
+   '(:eval (propertize (if overwrite-mode "Ovr" "Ins")
+		       'face 'font-lock-preprocessor-face
+		       'help-echo (concat "Buffer is in "
+					  (if overwrite-mode "overwrite" "insert") " mode")))
 
-               ;; was this buffer modified since the last save?
-               '(:eval (when (buffer-modified-p)
-                         (concat ","  (propertize "Mod"
-                                                  'face 'font-lock-warning-face
-                                                  'help-echo "Buffer has been modified"))))
+   ;; was this buffer modified since the last save?
+   '(:eval (when (buffer-modified-p)
+	     (concat ","  (propertize "Mod"
+				      'face 'font-lock-warning-face
+				      'help-echo "Buffer has been modified"))))
 
-               ;; is this buffer read-only?
-               '(:eval (when buffer-read-only
-                         (concat ","  (propertize "RO"
-                                                  'face 'font-lock-type-face
-                                                  'help-echo "Buffer is read-only"))))
-               "] "
+   ;; is this buffer read-only?
+   '(:eval (when buffer-read-only
+	     (concat ","  (propertize "RO"
+				      'face 'font-lock-type-face
+				      'help-echo "Buffer is read-only"))))
+   "] "
 
-	       ;;global-mode-string
-	       org-mode-line-string
-	       
-               ;; add the time, with the date and the emacs uptime in the tooltip
-               ;;'(:eval (propertize (format-time-string "%H:%M")
-               ;;                    'help-echo
-               ;;                    (concat (format-time-string "%c; ")
-               ;;                            (emacs-uptime "Uptime:%hh"))))
-               " --"
-	       
-               ;; i don't want to see minor-modes; but if you want, uncomment this:
-               ;;minor-mode-alist  ;; list of minor modes
-               "%-" ;; fill with '-'
-               ))
+   ;;global-mode-string
+   (eval-after-load 'org-mode org-mode-line-string)
+   
+   ;; add the time, with the date and the emacs uptime in the tooltip
+   ;;'(:eval (propertize (format-time-string "%H:%M")
+   ;;                    'help-echo
+   ;;                    (concat (format-time-string "%c; ")
+   ;;                            (emacs-uptime "Uptime:%hh"))))
+   " --"
+   
+   ;; i don't want to see minor-modes; but if you want, uncomment this:
+   ;;minor-mode-alist  ;; list of minor modes
+   "%-" ;; fill with '-'
+   )
+  )
 
 (provide 'molokai-theme-kit)
