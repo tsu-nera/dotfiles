98695dff5b1b3cade03e263e0cb950f4ed8c78d9
tsu-nera <fox10225fox@gmail.com>
2014-04-03 22:32:23 +0900

    vbasense追加

--- .emacs.d/inits/33_minorlang.el
+++ .emacs.d/inits/33_minorlang.el
@@ -13,4 +13,18 @@
 (autoload 'cool-mode "cool-mode" "Major mode for editing COOL programs" t)
 (setq auto-mode-alist
             (append '(("\\.cl\\'" . cool-mode)) auto-mode-alist))
-	  
+
+;; -----------------------------------------------------------------------
+;; Name     :  VBA
+;; ------------------------------------------------------------------------
+(require 'vbasense)
+
+;; キーバインド
+(setq vbasense-popup-help-key "C-:")
+(setq vbasense-jump-to-definition-key "C->")
+
+;; 必要に応じて適宜カスタマイズして下さい。以下のS式を評価することで項目についての情報が得られます。
+;; (customize-group "vbasense")
+
+;; 推奨設定を行う
+(vbasense-config-default)(require 'vbasense)
