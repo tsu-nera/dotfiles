0ba68f6cf8446ee4ee713169fd1be80793ec79cd
tsu-nera <fox10225fox@gmail.com>
2014-04-05 13:53:16 +0900

    パッケージ整理

--- .emacs.d/inits/00_package_manager.el
+++ .emacs.d/inits/00_package_manager.el
@@ -38,6 +38,8 @@
 (el-get 'sync
 	'helm
 	'yasnippet
+	'vbasense
+	'visual-basic-mode
 	'ruby-block
 	'robe-mode
 	'ruby-electric
@@ -59,7 +61,6 @@
 	'color-theme
 	'smart-compile
 	'helm-descbinds
-	'auto-install
 	'anything
 	'gist
 	'flymake
@@ -78,26 +79,7 @@
 	'org-mode
 	'org-pomodoro
 	'rainbow-mode
-	'vbasense
-	'visual-basic-mode
 	)
-
-;; ------------------------------------------------------------------------
-;; Name     : auto-install
-;; Function :
-;; History  :
-;; Install  : http://www.emacswiki.org/emacs/download/auto-install.el
-;; ------------------------------------------------------------------------
-(when(require 'auto-install nil t)
-  ;;インストールディレクトリを設定する  初期値は~/.emacs.d/auto-install/
-  (setq auto-install-directory "~/.emacs.d/elisp/")
-  ;;EmacsWikiに登録されているelispの名前を取得する
-  ;;; 起動時にnetwork unreachableってでるので、とりあえず封印 13/05/26
-  ;;(auto-install-update-emacswiki-package-name t)
-  ;;必要であればプロキシの設定を行う
-  ;;(setq url-proxy-services '(("http" . "localhost:8339")))
-  ;;install-elispの関数を利用可能にす
-  (auto-install-compatibility-setup))
 
 ;; ------------------------------------------------------------------------
 ;; Name     : package.el
