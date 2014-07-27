7077a339168bd3b8c57f4702a9d82a1a1a597f7c
tsu-nera <fox10225fox@gmail.com>
2014-07-27 23:00:30 +0900

    add gradle-mode

--- .emacs.d/inits/02_custom.el
+++ .emacs.d/inits/02_custom.el
@@ -4,19 +4,31 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#242424" "#E5786D" "#95E454" "#CAE682" "#8AC6F2" "#333366" "#CCAA8F" "#F6F3E8"])
  '(anzu-deactivate-region t)
  '(anzu-mode-lighter "")
  '(anzu-replace-to-string-separator " => ")
  '(anzu-search-threshold 1000)
  '(anzu-use-mimego t)
- '(custom-safe-themes (quote ("2b484c630af2578060ee43827f4785e480e19bab336d1ccb2bce5c9d3acfb652" "ea4035bd249cc84f038158d1eb17493623c55b0ca92d9f5a1d036d2837af2e11" "9fd20670758db15cc4d0b4442a74543888d2e445646b25f2755c65dcd6f1504b" default)))
+ '(custom-enabled-themes nil)
+ '(custom-safe-themes
+   (quote
+    ("9d5a88f5a74e0be7c259f3ece3104e578ae585abba5a62d0136d1103a38bb449" "2b484c630af2578060ee43827f4785e480e19bab336d1ccb2bce5c9d3acfb652" "ea4035bd249cc84f038158d1eb17493623c55b0ca92d9f5a1d036d2837af2e11" "9fd20670758db15cc4d0b4442a74543888d2e445646b25f2755c65dcd6f1504b" default)))
  '(ecb-options-version "2.40")
  '(elscreen-tab-display-control nil)
  '(elscreen-tab-display-kill-screen nil)
+ '(helm-gtags-auto-update t)
+ '(helm-gtags-ignore-case t)
+ '(helm-gtags-path-style (quote relative))
+ '(init-loader-show-log-after-init (quote error-only))
  '(org-agenda-files (quote ("~/gtd/main.org")))
+ '(robe-highlight-capf-candidates nil)
  '(rspec-use-rake-when-possible nil)
  '(safe-local-variable-values (quote ((require-final-newline . t))))
- '(vbasense-tli-files (quote ("c:\\Program Files (x86)\\Microsoft Office\\OFFICE14\\EXCEL.EXE" "c:/Program Files (x86)/Common Files/Microsoft Shared/VBA/VBA7/VBE7.DLL" "c:/Program Files (x86)/Common Files/Microsoft Shared/VBA/VBA6/VBE6EXT.OLB" "c:/Program Files (x86)/Common Files/Microsoft Shared/OFFICE14/MSO.DLL" "C:\\Windows\\SysWOW64\\stdole2.tlb")))
+ '(vbasense-tli-files
+   (quote
+    ("c:\\Program Files (x86)\\Microsoft Office\\OFFICE14\\EXCEL.EXE" "c:/Program Files (x86)/Common Files/Microsoft Shared/VBA/VBA7/VBE7.DLL" "c:/Program Files (x86)/Common Files/Microsoft Shared/VBA/VBA6/VBE6EXT.OLB" "c:/Program Files (x86)/Common Files/Microsoft Shared/OFFICE14/MSO.DLL" "C:\\Windows\\SysWOW64\\stdole2.tlb")))
  '(visual-basic-mode-indent 2)
  '(yas-trigger-key "TAB"))
 
