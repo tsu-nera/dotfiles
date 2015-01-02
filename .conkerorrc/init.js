46725baf7bf71139e70f4758dccaed037f9aa95c
tsu-nera <fox10225fox@gmail.com>
2015-01-03 01:31:19 +0900

    change .conkerorrc

--- .conkerorrc/init.js
+++ .conkerorrc/init.js
@@ -116,26 +116,26 @@ cwd.append("Downloads");
 //////////////////////////////////////////
 
 // Darken Pages  very cool staff!
-function darken_page (I) {
-    var styles='* { background: black !important; color: grey !important; }'+
-        ':link, :link * { color: #4986dd !important; }'+
-        ':visited, :visited * { color: #d75047 !important; }';
-    var document = I.buffer.document;
-    var newSS=document.createElement('link');
-    newSS.rel='stylesheet';
-    newSS.href='data:text/css,'+escape(styles);
-    document.getElementsByTagName("head")[0].appendChild(newSS);
-}
+// function darken_page (I) {
+//     var styles='* { background: black !important; color: grey !important; }'+
+//         ':link, :link * { color: #4986dd !important; }'+
+//         ':visited, :visited * { color: #d75047 !important; }';
+//     var document = I.buffer.document;
+//     var newSS=document.createElement('link');
+//     newSS.rel='stylesheet';
+//     newSS.href='data:text/css,'+escape(styles);
+//     document.getElementsByTagName("head")[0].appendChild(newSS);
+// }
 
-interactive("darken-page", "Darken the page in an attempt to save your eyes.",
-            darken_page);
+// interactive("darken-page", "Darken the page in an attempt to save your eyes.",
+//             darken_page);
 
-define_key(content_buffer_normal_keymap, "C-d", "darken-page");
+// define_key(content_buffer_normal_keymap, "C-d", "darken-page");
 
 // color theme zenburn
 // https://github.com/ivoarch/conkeror-theme-zenburn
 // font-size is rather small. +3 is suitable for me :)
 //	
-theme_load_paths.unshift("~/.conkeror/themes/");
+theme_load_paths.unshift("~/.conkerorrc/themes/");
 theme_unload("default");
 theme_load("conkeror-theme-zenburn");
