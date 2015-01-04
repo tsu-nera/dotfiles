8ba474a34f4b68e9a17b1564a0b4f4799903ae88
tsu-nera <fox10225fox@gmail.com>
2015-01-04 23:41:36 +0900

    add custom file

---
+++ .emacs.d/inits/02_custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(rspec-use-rake-when-possible nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ac-emacs-eclim-candidate-face ((t (:inherit ac-candidate-face))))
+ '(ac-emacs-eclim-selection-face ((t (:inherit ac-selection-face))))
+ '(elscreen-tab-control-face ((t (:background "#1c1c1c" :foreground "#9e9e9e" :underline t))))
+ '(elscreen-tab-current-screen-face ((t (:background "#444444" :foreground "#9e9e9e"))))
+ '(elscreen-tab-other-screen-face ((t (:background "#262626" :foreground "#9e9e9e" :underline t))))
+ '(font-lock-comment-delimiter-face ((t (:inherit font-lock-comment-face :foreground "gray80"))))
+ '(font-lock-comment-face ((t (:foreground "gray80"))))
+ '(minibuffer-prompt ((t (:foreground "cyan")))))
