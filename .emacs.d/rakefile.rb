08b96dd45d2a01e95727cc0ba236d2302b901f17
tsu-nera <fox10225fox@gmail.com>
2015-01-10 20:37:28 +0900

    add archlinux playbook

--- .emacs.d/rakefile.rb
+++ .emacs.d/rakefile.rb
@@ -1,6 +1,6 @@
 require 'rake/clean'
 CLEAN.include(["**/*~$", "**/#*"])
-CLOBBER.include(["inits/*.el","**/*.elc"])
+CLOBBER.include(["inits/*.el","inits/*.elc"])
 
 desc "reboot emacs"
 task :reboot => :clobber do
@@ -11,4 +11,3 @@ desc "reboot emacs twice"
 task :reboot2 => :reboot do
   system("emacsclient -e \"(kill-emacs)\";emacs --daemon")
 end
-
