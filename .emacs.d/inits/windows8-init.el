f0b1ab7c7e98c3819b7b4bfadee95634b031b509
tsu-nera <fox10225fox@gmail.com>
2015-07-28 21:14:32 +0900

    よくわからないけど、アップデート

--- .emacs.d/inits/windows8-init.el
+++
@@ -1,4 +0,0 @@
-(setq org-agenda-files '("c:/Users/tsu-nera/Dropbox/gtd/main.org"
-                         "c:/Users/tsu-nera/Dropbox/gtd/inbox.org"
-                         "c:/Users/tsu-nera/Dropbox/gtd/journal/journal.org"
-                         "c:/Users/tsu-nera/Dropbox/gtd/habits.org"))
