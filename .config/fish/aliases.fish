20a49e7ef36c146dad0b30a7364323a183bdd022
tsu-nera <fox10225fox@gmail.com>
2016-12-24 16:48:14 +0900

    fish設定

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -6,8 +6,11 @@ alias xmap 'xmodmap ~/.Xmodmap'
 # Emacs関連
 alias m 'emacsclient -nw'
 alias kill_emacs "emacsclient -e \"(kill-emacs)\""
-alias boot_emacs "LC_CTYPE=ja_JP.UTF-8 emacs --daemon"
-alias reboot_emacs "kill_emacs;boot_emacs"
+alias boot_emacs "emacs --daemon"
+
+function reboot_emacs
+	 kill_emacs;boot_emacs
+end
 
 # 画面ロック
 alias lock 'gnome-screensaver-command -l'
@@ -19,6 +22,23 @@ alias fault 'sudo shutdown -P now'
 # function fish_prompt
 #     ~/powerline-shell.py $status --shell bare ^/dev/null
 # end
+
+
+#######################################
+## peco
+######################################
+function peco
+  command peco --layout=bottom-up $argv
+end
+
+function fish_user_key_bindings
+        # ghqを選択
+        bind \cl peco_select_ghq_repository
+        # コマンド履歴を見る
+        bind \cr peco_select_history
+        # プロセスをキルする
+        bind \cx\ck peco_kill        
+end
 
 #######################################################
 # multi-display
@@ -35,4 +55,15 @@ end
 # set single monitor
 function single
     xrandr --output HDMI1 --off
+end
+
+############
+## Emacs
+###########
+function dired 
+        emacsclient -e "(dired \"$PWD\")"
+end
+
+## Chdir to the ``default-directory'' of currently opened in Emacs buffer.
+function cde
 end
--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -4,14 +4,25 @@ export LIBRARY_PATH=/usr/local/lib
 export CPATH=/usr/local/include
 
 if test -d ~/script
-        set -U fish_user_paths ~/script/sh $fish_user_paths
-        set -U fish_user_paths ~/script/ruby $fish_user_paths
-        set -U fish_user_paths ~/script/scala $fish_user_paths        
+        set fish_user_paths ~/script/sh $fish_user_paths
+        set fish_user_paths ~/script/ruby $fish_user_paths
+        set fish_user_paths ~/script/scala $fish_user_paths        
 end
 
 set -x EDITOR emacs
+set -x VISUAL "emacsclient -nw"
 
 # for ruby
 if test -x "`which ruby`"
-        export fish_user_paths $HOME/.gem/ruby/2.1.0/bin fish_user_paths
+        set fish_user_paths $HOME/.gem/ruby/2.1.0/bin $fish_user_paths
 end
+
+# for go
+if test -x "`which go`"
+        set -x GOPATH $HOME/go
+        set fish_user_paths $GOROOT/bin $fish_user_paths 
+        set fish_user_paths $GOPATH/bin $fish_user_paths 
+end
+
+# my bin
+set fish_user_paths $HOME/bin $fish_user_paths
