04fde0e52f11412d4af6cc9bbae50e4fe9654a7e
tsu-nera <fox10225fox@gmail.com>
2017-01-12 12:51:51 +0900

    いつの前にか紛れ込んだゴミを削除

--- Linux.fish
+++
@@ -1,3 +0,0 @@
-# Functions specific to Linux users
-# Please note that this file is version controlled. 
-# Use one of the local files (hostname or username) for user/host specific settings
--- functions/fg.fish
+++
@@ -1,3 +0,0 @@
-function fg
-	f $argv[1] | xargs grep $argv[2]
-end
