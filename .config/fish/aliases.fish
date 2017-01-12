2ba65690f302124a12d9aa76e614bb9c32981015
tsu-nera <fox10225fox@gmail.com>
2017-01-12 10:36:02 +0900

    fzf alias

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -44,6 +44,13 @@ function fish_user_key_bindings
         bind \cx\ck peco_kill
         # 最近見たディレクトリに移動
         bind \cx\cr peco_recentd
+
+        # fzf
+        bind \cf '__fzf_find_file'
+        bind \ct '__fzf_reverse_isearch'
+        bind \ex '__fzf_find_and_execute'
+        bind \ed '__fzf_cd'
+        bind \eD '__fzf_cd_with_hidden'
 end
 #######################################################
 # multi-display
